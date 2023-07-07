cbfa38204a44e7c27dfe871bad609f773eeb1913
bitmap357 <edakoyeram@gmail.com>
2023-07-07 15:39:07 +0000

    url entry

--- Video Downloader.py
+++ Video Downloader.py
@@ -11,4 +11,12 @@ canvas.pack()
 # App Label
 app_label = Label(root, text="Video Downloader", fg="blue", font=('Ariel', 20))
 canvas.create_window(200, 20, window=app_label)
+
+# Entry to accept Video url
+url_label = Label(root, text='Enter video url')
+url_entry = Entry(root)
+canvas.create_window(200, 80, window=url_label)
+canvas.create_window(200, 100, window=url_entry)
+
+
 root.mainloop()
