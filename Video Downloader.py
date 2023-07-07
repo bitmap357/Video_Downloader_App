d2a405d064e82ea689cea70095e9ef7014a7ee3a
bitmap357 <edakoyeram@gmail.com>
2023-07-07 15:35:53 +0000

    Label

--- Video Downloader.py
+++ Video Downloader.py
@@ -7,4 +7,8 @@ root. title('Video Downloader')
 canvas = Canvas(root, width=400, height=300)
 canvas.pack()
 
+
+# App Label
+app_label = Label(root, text="Video Downloader", fg="blue", font=('Ariel', 20))
+canvas.create_window(200, 20, window=app_label)
 root.mainloop()
